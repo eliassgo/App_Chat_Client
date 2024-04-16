52747f526156c7e09b22b0bac0cb3b3008ea7318
Elias Gomes <eliasgo221@gmail.com>
2024-04-16 07:57:21 -0300

    Add connection with mongoDB

--- server.js
+++ server.js
@@ -1,31 +1,55 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
-var http = require('https').Server(app)
+var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'}, 
-    {name: 'Jane', message: 'Hello'}
-]
+var dbUrl = 'mongodb+srv://user:user@appnode.xk12iyk.mongodb.net/learning-node'
 
-app.get('/messages', (req, res) => {
-    res.send(messages)
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
+app.get('/messages', (req, res) => {
+    Message.find({}).then((messages) => {
+        res.send(messages)
+    })
+    .catch((err) =>{
+        console.error(err);
+        res.sendStatus(500);
+    })
+})
+
+
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    
+    message.save().then(() => {
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        })
+        .catch((err) => {
+            res.sendStatus(500)
+        })
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 
 var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
